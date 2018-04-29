a6afcea3a2255527a7995154039bb9955a2cccbc
m <mathuang116@gmail.com>
2018-04-28 21:22:26 -0700

    creat react template

--- projects/realstate/src/app.js
+++ projects/realstate/src/app.js
@@ -1,44 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import AppRouter, { history } from './routers/AppRouter';
-import configureStore from './store/configureStore';
-import { login, logout } from './actions/auth';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-import 'react-dates/lib/css/_datepicker.css';
-import { firebase } from './firebase/firebase';
-import LoadingPage from './components/LoadingPage';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const store = configureStore();
-const jsx = (
-  <Provider store={store}>
-    <AppRouter />
-  </Provider>
-);
-ReactDOM.render(jsx, document.getElementById('app'));
-/*
-let hasRendered = false;
-const renderApp = () => {
-  if (!hasRendered) {
-    ReactDOM.render(jsx, document.getElementById('app'));
-    hasRendered = true;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        hello world
+      </div>
+    );
   }
-};
+}
 
-ReactDOM.render(<LoadingPage />, document.getElementById('app'));
-
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    store.dispatch(login(user.uid));
-    renderApp();
-    if (history.location.pathname === '/') {
-      history.push('/dashboard');
-    }
-  } else {
-    store.dispatch(logout());
-    renderApp();
-    history.push('/');
-  }
-});
-*/
+export default App;
