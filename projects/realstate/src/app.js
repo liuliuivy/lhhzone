aa0979de9511e0812c7d33c428267f50b3efee41
m <mathuang116@gmail.com>
2018-05-02 22:18:19 -0700

    want to sell

--- projects/realstate/src/app.js
+++ projects/realstate/src/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Nav from './components/Nav';
 import MostPopular from './components/MostPopular';
 import Slide from './components/Slide';
+import WantToSell from './components/WantToSell';
 import "./common/css/bootstrap.css";
 import "./common/css/jquery.fancybox.css";
 import "./common/css/style.css";
@@ -14,6 +15,7 @@ class App extends Component {
         <Nav/>
         <Slide/>
         <MostPopular/>
+        <WantToSell/>
       </div>
     );
   }
