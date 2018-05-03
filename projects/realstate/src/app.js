0871e8a2ccf7f9713467781820c3f4c6ac3a75a1
liuliuivy <ivyliu0408@gmail.com>
2018-05-02 19:50:29 -0700

    smart detail

--- projects/realstate/src/app.js
+++ projects/realstate/src/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Nav from './components/Nav';
 import MostPopular from './components/MostPopular';
 import Slide from './components/Slide';
+import SmartDetails from './components/SmartDetails';
 import "./common/css/bootstrap.css";
 import "./common/css/jquery.fancybox.css";
 import "./common/css/style.css";
@@ -12,6 +13,7 @@ class App extends Component {
     return (
       <div>
         <Nav/>
+        
         <Slide/>
         <MostPopular/>
       </div>
