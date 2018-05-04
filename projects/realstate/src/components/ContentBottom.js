5b0e98695a9b6a1da72d11e964add32f400e3666
m <mathuang116@gmail.com>
2018-05-03 22:29:59 -0700

    add middle

--- projects/realstate/src/components/ContentBottom.js
+++ projects/realstate/src/components/ContentBottom.js
@@ -4,12 +4,14 @@ import "../common/css/jquery.fancybox.css";
 import "../common/css/style.css";
 import SaleRent from "./SaleRent";
 import Agent from "./Agent" ;
+import Middle from "./Middle" ;
 
 class ContentBottom extends Component{
     render(){
         return(
         <div className="content_bottom">
             <SaleRent/>
+            <Middle/>
             <Agent/>
             
 
